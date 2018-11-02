3d992934b2cffd7ae1d13c7c7addb26a158f84ee
AnssamGhezala <anssam.ghezala@mail.mcgill.ca>
2018-11-02 05:10:30 -0400

    add correct version of src file

--- src/App.js
+++ src/App.js
@@ -8,38 +8,49 @@ class App extends Component {
   interval = {};
  
   clearEvent = () => {
+    console.log("clear event");
     this.setState({counter:0,running:false});
-    clearInterval(this.interval);
+    clearInterval(this.interval);    
 }
 
-  startEvent = () => {
-  
-    var time = 0;   
+   stopEvent = () => {
  
+   
+   var lastCounter = this.state.counter;
+   this.setState({counter:lastCounter,running:false});
+   clearInterval(this.interval); 
+
+   }
+
+  startEvent = () => {
+    
+
+
+    var time = 0;   
+
         if(this.state.running == false) {
-           
+          this.setState({running:true});
            this.interval = setInterval(() => {
-            time = time+1;
+            
+            time = this.state.counter+1;
             this.setState({counter:time});   
             });
-
-        } else {
-            this.clearEvent();
-        }
-        
+        } 
+       
   } 
 
 
   render() {
     const element = this.state;
+    const nowTime = element.counter;
     return (
-      <div>
-      <label>{element.counter}ms</label>
-      <button onClick={this.startEvent} className="startBtn">Start</button>
-     {/* <button onClick={this.stopEvent} className="stopBtn" >Stop</button> */}
-      <button onClick={this.clearEvent} className="clearBtn" >Clear</button>
+      <center className="stopwatch">
+      <label className="rounded-circle border border-primary"> {element.counter} ms </label>
+      <button onClick={this.startEvent} className="btn btn-outline-success startBtn">Start</button>
+      <button onClick={this.stopEvent} className="btn btn-outline-dark stopBtn" >Stop</button> 
+      <button onClick={this.clearEvent} className="btn btn-outline-danger clearBtn" >Clear</button>
       
-      </div>
+      </center>
     );
   }
 }
